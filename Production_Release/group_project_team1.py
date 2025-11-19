6db4662e391cdd312b707262f551bea3fa6eb80c
CLee2124 <clee24@ramapo.edu>
2025-11-18 22:45:19 -0500

    Create group_project_team1.py

---
+++ Production_Release/group_project_team1.py
@@ -0,0 +1,20 @@
+"""
+INFO-233 Group Project
+Project Name: Stock Portfolio Builder
+Team Name: Spyder01
+Group 1
+"""
+# Modules
+#import
+
+# Functions
+def stock_api():
+    pass
+def mail_api():
+    pass
+def calc_stock():
+    pass
+def main():
+    pass
+if __name__ =="__main__":  
+	main()
