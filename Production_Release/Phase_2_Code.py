e23fca1d103e83442bdad5eace45982a8ddf8be4
CLee2124 <clee24@ramapo.edu>
2025-11-21 09:24:59 -0500

    Update Phase_2_Code.py

--- Production_Release/Phase_2_Code.py
+++ Production_Release/Phase_2_Code.py
@@ -30,6 +30,6 @@ def main(): # Main Program
             print("Please enter a valid input.")
 #    stock_info = stock_api(choice) # get 4 stock prices and info
 #    calc_stock(investment, stock_info) # Calculate invesment split between stocks
-#    mail_api # email stock portfolio to user
+#    mail_api() # email stock portfolio to user
 if __name__ =="__main__":  
 	main()
