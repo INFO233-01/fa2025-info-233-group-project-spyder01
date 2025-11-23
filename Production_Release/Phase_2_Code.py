4744b99c16aaf0ce8ab2817b4977ff22ac258196
CLee2124 <clee24@ramapo.edu>
2025-11-23 16:53:28 -0500

    Update Phase_2_Code.py

--- Production_Release/Phase_2_Code.py
+++ Production_Release/Phase_2_Code.py
@@ -9,36 +9,32 @@ import requests
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 # Functions
-def stock_api(): # Stock API
-    pass 
-API_KEY="API KEY GOES HERE"
-URL = f"https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey={API_KEY}"
 def stock_api(choice):
-response = requests.get(URL)
-data = response.json()
-
-low = data.get("top_losers", [])
-active = data.get("most_actively_traded", [])
-high = data.get("top_gainers", [])
-
-if choice == "low":
-    return low[:4]
-elif choice == "active":
-    return active[:4]
-elif choice == "high":
-    return high[:4]
-return []
+    API_KEY = ("WUYXNSIWB11JPE9V")
+    URL = (f"https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey={API_KEY}")
+    response = requests.get(URL)
+    data = response.json()
+    low = data.get("top_losers", [])
+    active = data.get("most_actively_traded", [])
+    high = data.get("top_gainers", [])
+    if choice == "low":
+        return low[:4]
+    elif choice == "active":
+        return active[:4]
+    elif choice == "high":
+        return high[:4]
+    return []
 def mail_api(): # Email API
-    message = Mail(from_email = 'whatevermail@gmail.com',
-    				to_emails ='whateveremail@gmail.com',
+    message = Mail(from_email = 'clee24@ramapo.edu',
+    				to_emails ='clee24@ramapo.edu',
     				subject ='Testing the SendGrid API for our group project',
     				html_content ='Hello, this is to see if I called the API correctly.')
-	sg = SendGridAPIClient(api_key='') 
-	response = sg.send(message)
-	print(response.status_code, response.body)
+    sg = SendGridAPIClient(api_key='') 
+    response = sg.send(message)
+    print(response.status_code, response.body)
 def calc_stock(investment, stock_info): # Calculate cost and number of stocks
     pass
-    # cost = stock_info[4 stocks] # The cost to buy 1 of each stock
+    # cost = stock_info[ticker:price] # The cost to buy 1 of each stock
     # stock_amount = investment / cost # Number of each stock purchased
     # total = stock_amount * cost # Total cost of purchased stocks
     # return stock_amount, total # Returns total and number of stocks
@@ -60,8 +56,9 @@ def main(): # Main Program
                 print("Please enter a valid input.")
         except ValueError:
             print("Please enter a valid input.")
-#    stock_info = stock_api(choice) # get 4 stock prices and info
+    stock_info = stock_api(choice) # get 4 stock prices and info
+    print(stock_info)
 #    stock_amount, total_cost = calc_stock(investment, stock_info) # Calculate invesment split between stocks
-#    mail_api # email stock portfolio to user
+    mail_api # email stock portfolio to user
 if __name__ =="__main__":  
 	main()
